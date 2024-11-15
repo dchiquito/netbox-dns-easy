02e3c7791128dee82df5e062e5ca16c911098a63
Daniel Chiquito <daniel.chiquito@gmail.com>
2024-11-14 22:11:20 -0500

    Fix plugin name

--- netbox_dns_easy/__init__.py
+++ netbox_dns_easy/__init__.py
@@ -2,9 +2,9 @@ from netbox.plugins import PluginConfig
 
 
 class NetBoxAccessListsConfig(PluginConfig):
-    name = "netbox_access_lists"
-    verbose_name = " NetBox Access Lists"
-    description = "Manage simple ACLs in NetBox"
+    name = "netbox_dns_easy"
+    verbose_name = "Easy DNS for NetBox"
+    description = "A simple interface for DNS management in NetBox"
     version = "0.1"
     base_url = "access-lists"
 
