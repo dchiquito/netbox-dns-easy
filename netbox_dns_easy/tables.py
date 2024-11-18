e9b8aa4d2bcc02259ee9418e0b91ed1a60e779e4
Daniel Chiquito <daniel.chiquito@gmail.com>
2024-11-18 13:41:37 -0500

    Working table

--- netbox_dns_easy/tables.py
+++ netbox_dns_easy/tables.py
@@ -3,25 +3,40 @@ from django.utils.translation import gettext_lazy as _
 
 from netbox.tables import NetBoxTable, columns
 from dcim.models import Device
+from netbox_dns.models import Record
 
 
 class DeviceDomainTable(NetBoxTable):
-    name = tables.Column(linkify=True)
-    status = columns.ChoiceFieldColumn(
+    ipam_ip = tables.Column(
+        linkify=("ipam:ipaddress", [tables.A("ipam_ip_pk")]))
+    device_name = tables.Column(
+        linkify=("dcim:device", [tables.A("device_pk")]))
+    device_status = columns.ChoiceFieldColumn(
         verbose_name=_("Status"),
     )
+    device_tenant = tables.Column(
+        linkify=("tenancy:tenant", [tables.A("device_tenant_pk")])
+    )
+    device_site = tables.Column(
+        linkify=("dcim:site", [tables.A("device_site_pk")]))
 
     class Meta(NetBoxTable.Meta):
-        model = Device
+        model = Record
         fields = (
-            "pk",
-            "id",
-            "name",
-            "status",
-            "tenant",
-            "site",
-            "primary_ip",
+            # "pk",
+            # "id",
             "dns_name",
+            "ipam_ip",
+            "device_name",
+            "device_status",
+            "device_tenant",
+            "device_site",
         )
-        default_columns = ("name", "status", "tenant",
-                           "site", "primary_ip", "dns_name")
+        default_columns = (
+            "dns_name",
+            "ipam_ip",
+            "device_name",
+            "device_status",
+            "device_tenant",
+            "device_site",
+        )
--- netbox_dns_easy/views.py
+++ netbox_dns_easy/views.py
@@ -1,5 +1,6 @@
 from django.db.models import F
 from dcim.models import Device
+from ipam.models import IPAddress
 from netbox.views import generic
 from netbox_dns.models import Record
 
@@ -10,5 +11,36 @@ class DeviceDomainsView(generic.ObjectListView):
     # TODO start with managed records and work back to the Device
     # Record.objects.filter(managed=True)
     # .ipam_ip_address.assigned_object.parent_object
-    queryset = Device.objects.all().annotate(dns_name=F("primary_ip4__dns_name"))
+    # address_records = ip_address.netbox_dns_records.all()
+    # queryset = Device.objects.all().annotate(dns_name=F("primary_ip4__dns_name"))
+    def get_queryset(self, request):
+        # ip_addresses = (
+        #     row[0]
+        #     for row in Record.objects.filter(managed=True).values_list(
+        #         "ipam_ip_address"
+        #     )
+        # )
+
+        return (
+            Record.objects.filter(managed=True)
+            .exclude(ipam_ip_address=None)
+            .alias(device=F("ipam_ip_address__interface__device"))
+            .annotate(
+                dns_name=F("ipam_ip_address__dns_name"),
+                ipam_ip_pk=F("ipam_ip_address__pk"),
+                ipam_ip=F("ipam_ip_address__address"),
+                device_pk=F("ipam_ip_address__interface__device__pk"),
+                device_name=F("ipam_ip_address__interface__device__name"),
+                device_status=F("ipam_ip_address__interface__device__status"),
+                device_tenant_pk=F(
+                    "ipam_ip_address__interface__device__tenant__pk"),
+                device_tenant=F(
+                    "ipam_ip_address__interface__device__tenant__name"),
+                device_site_pk=F(
+                    "ipam_ip_address__interface__device__site__pk"),
+                device_site=F(
+                    "ipam_ip_address__interface__device__site__name"),
+            )
+        )
+
     table = tables.DeviceDomainTable
